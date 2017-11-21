b2de0e06b08b42c1727907ede7695e16852d09bd
SirJacobB <jacob.brillante.te15d@gmail.com>
2017-11-21 10:11:55 +0100

    created class, added getters and setters

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+
+public class Person{
+
+	private String fname;
+	private String lname;
+	
+	Person(String fullName){
+		
+	}
+	
+	public String getfName() {
+		return fname;
+	}
+	
+	public void setfName(String fName) {
+		this.fname = fName;
+	}
+	
+	public String getlName() {
+		return lname;
+	}
+	
+	public void setlName(String lName) {
+		this.lname = lName;
+	}
+}
