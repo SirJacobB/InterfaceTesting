74e0e92c918118823ea04b02824ad079d49c34e5
SirJacobB <jacob.brillante.te15d@gmail.com>
2017-11-21 10:28:56 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Person p = new Person("Johan Ohlsson");
+		
+		System.out.println(p.getfName());
+		System.out.println(p.getlName());
 	}
 }
