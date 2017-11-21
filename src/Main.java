768aae2c40a6f5bad505c57ce56c0c364dbe7201
SirJacobB <jacob.brillante.te15d@gmail.com>
2017-11-21 10:43:07 +0100

    ignore

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Person p = new Person("Johan Ohlsson");
+		Person p = new Person("Ohlsson Wall Johan");
 		
 		System.out.println(p.getfName());
 		System.out.println(p.getlName());
